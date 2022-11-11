9669f538e69c51ad08f2a3973080abb8013b83cf
tcharlton21 <102935740+tcharlton21@users.noreply.github.com>
2022-11-11 13:55:29 -0600

    Changed color scheme and legend

--- OneGenMap.py
+++ OneGenMap.py
@@ -6,13 +6,13 @@ with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-c
 
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/tcharlton21/OneGenAwayMap/main/Food%20Insecure%25.csv",
+df = pd.read_csv("https://raw.githubusercontent.com/tcharlton21/OneGenMapPython/main/Food%20Insecure%25.csv",
                  dtype={"FIPS": str})
 
 import plotly.express as px
 
-fig = px.choropleth_mapbox(df, geojson=counties, locations='FIPS', color='Overall Food Insecurity Rate (1 Year)',
-                           color_continuous_scale="Viridis",
+fig = px.choropleth_mapbox(df, geojson=counties, locations='FIPS', color='Food Insecurity %',
+                           color_continuous_scale="YlOrRd",
                            range_color=(0, 30),
                            mapbox_style="carto-positron",
                            zoom=3, center={"lat": 37.0902, "lon": -95.7129},
