c0250f63036f125a0f27df3250d9e54a3e7ac6b1
tcharlton21 <102935740+tcharlton21@users.noreply.github.com>
2022-11-11 14:05:15 -0600

    Changed zoom

--- OneGenMap.py
+++ OneGenMap.py
@@ -15,7 +15,7 @@ fig = px.choropleth_mapbox(df, geojson=counties, locations='FIPS', color='Food I
                            color_continuous_scale="YlOrRd",
                            range_color=(0, 30),
                            mapbox_style="carto-positron",
-                           zoom=3, center={"lat": 37.0902, "lon": -95.7129},
+                           zoom=5.75, center={"lat": 34.7490, "lon": -84.3880},
                            opacity=0.5,
                            labels={'# of Food Insecure Persons Overall (1 Year)': 'Total Food Insecure'}
                            )
